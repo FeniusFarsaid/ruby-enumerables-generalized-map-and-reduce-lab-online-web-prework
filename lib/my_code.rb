25ac90f41b39d99d82df8a196a996b9637fa0771
David Ingram <david@ingramaviation.com>
2019-07-23 17:40:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -26,7 +26,7 @@ def reduce(source_array, starting_point = nil)
   end
   
   while i < source_array.length do    
-		memo = (yield(source_array[i]))    
+		memo = (yield(memo, source_array[i]))    
 		i += 1  
 	end 
   
