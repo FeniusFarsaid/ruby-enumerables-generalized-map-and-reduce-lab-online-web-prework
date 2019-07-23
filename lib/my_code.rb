3cee1fda39d8215f0de66ba3620efeee9da40d9f
David Ingram <david@ingramaviation.com>
2019-07-23 17:20:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,8 +7,7 @@
 def map(source_array)   
 	new = []   
 	i = 0   
-	while i < 
-	source_array.length do    
+	while i < source_array.length do    
 		new.push(yield(source_array[i]))     
 		i += 1   
 	end   
